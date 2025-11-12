f083b68cb03e8f18face4bcf73a593c24a3e683b
A737-com <archits847@gmail.com>
2025-11-12 16:02:47 -0600

    Update alert name to Archit Singh

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const inputField = document.getElementById('entryinput');
     const alertButton = document.getElementById('entrybutton');
     const outputText = document.getElementById('textoutput');
-    const yourName = "Archit Singh"; // replace with your name
+    const yourName = "Archit singh"; // replace with your name
 
     function showMessage() {
         const userInput = inputField.value.trim();
